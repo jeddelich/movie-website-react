6ecdffc54f43e58c6ed73fc7930843d141850599
jeddelich@gmail.com <jeddelich@gmail.com>
2026-01-26 12:14:46 -0500

    page-routes-completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Search from './pages/Search';
+import Landing from './pages/Landing';
+import Movie from './pages/Movie';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/:id' element={<Movie />} />
+      </Routes>
+    </Router>
   );
 }
 
