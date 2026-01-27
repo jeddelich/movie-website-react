49f8c573d447359494f5f20601136ba6bc97fda3
jeddelich@gmail.com <jeddelich@gmail.com>
2026-01-26 22:01:44 -0500

    navigation-home-and-link-reversal

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,26 @@
-import './Nav.css'
-import Logo from "../assets/Logo.png"
+import "./Nav.css";
+import Logo from "../assets/Logo.png";
+import { useNavigate } from "react-router-dom";
 
 function Nav() {
+  const navigate = useNavigate();
+
+  function returnHome() {
+    navigate("/");
+
+    // opposite of potential util function in app.js
+    const navLinks = document.querySelectorAll(".nav__link--1");
+    navLinks[0].style.display = "flex";
+    navLinks[1].style.display = "flex";
+    const navLinkHome = document.querySelector(".nav__link--2");
+    navLinkHome.style.display = "none";
+  }
+
   return (
     <nav>
       <div className="nav__side--left">
         <figure className="logo--wrapper">
-            <img src={Logo} alt="" className="logo" />
+          <img src={Logo} alt="" className="logo" />
         </figure>
       </div>
       <div className="nav__side--right">
@@ -14,16 +28,16 @@ function Nav() {
           <li className="nav__link nav__link--2">
             <a
               className="nav__link--anchor nav__link--anchor2"
-            //   onClick="window.location.reload();"
+              onClick={returnHome}
             >
               Return To Home Page
             </a>
             <div className="underline"></div>
-            <figure className='home__icon--wrapper'>
-            <i
-              className="fa-solid fa-arrow-right-to-bracket"
-            //   onClick="window.location.reload();"
-            ></i>
+            <figure className="home__icon--wrapper">
+              <i
+                className="fa-solid fa-arrow-right-to-bracket"
+                //   onClick="window.location.reload();"
+              ></i>
             </figure>
           </li>
           <li className="nav__link nav__link--1">
