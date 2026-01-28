c83f1c22394cec3e362ede6ff4f9a9c65e61e876
jeddelich@gmail.com <jeddelich@gmail.com>
2026-01-28 15:51:21 -0500

    form-completion-required-before-submission

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,18 +1,28 @@
-import "./ContactForm.css"
+import "./ContactForm.css";
 
-function ContactForm({setSubmit}) {
+function ContactForm({ setSubmit }) {
+    
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!event.currentTarget.checkValidity()) return;
+
+    setSubmit(true);
+  }
 
   return (
-    <form id="contact__form" action="">
-        <label htmlFor="">Name:</label>
-        <input type="text" required/>
-        <label htmlFor="">Email:</label>
-        <input type="email" required/>
-        <label htmlFor="">Message:</label>
-        <textarea name="" id="" required></textarea>
-        <button className="contact__form--submit" onClick={() => setSubmit(true)}>Send Message To Cinemations</button>
+    <form id="contact__form" action="" onSubmit={handleSubmit}>
+      <label htmlFor="">Name:</label>
+      <input type="text" required />
+      <label htmlFor="">Email:</label>
+      <input type="email" required />
+      <label htmlFor="">Message:</label>
+      <textarea name="" id="" required></textarea>
+      <button className="contact__form--submit" type="submit">
+        Send Message To Cinemations
+      </button>
     </form>
-  )
+  );
 }
 
-export default ContactForm
+export default ContactForm;
