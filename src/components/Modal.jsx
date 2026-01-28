dcaeadb7290821d8d12254c1827c60631d2aa5ba
jeddelich@gmail.com <jeddelich@gmail.com>
2026-01-28 11:30:31 -0500

    about-modal-completed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,19 @@
-import './Modal.css'
+import "./Modal.css";
 
 function Modal() {
   return (
     <div className="modal">
+      <div className="module__container">
+        <div className="module__row">
+          <h3 className="modal__title">Your Next Movie Is Our Mission</h3>
+          <p className="modal__para">
+            Whether it's for your next family movie night, a romantic date, or to discover your next binge-worthy TV series, Cinemations is here to help! With our vast database of
+            movies and TV shows, we guarantee you'll find all the info you need to pick your next hit movie!
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Modal
+export default Modal;
