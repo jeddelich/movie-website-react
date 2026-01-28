f5547e11b91ccff53f47cf5beff42286e83a1582
jeddelich@gmail.com <jeddelich@gmail.com>
2026-01-28 12:38:39 -0500

    modal-props-created

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,12 @@
 import "./Modal.css";
 
-function Modal() {
+function Modal({ modalTitle, modalPara}) {
   return (
     <div className="modal">
       <div className="module__container">
         <div className="module__row">
-          <h3 className="modal__title">Your Next Movie Is Our Mission</h3>
-          <p className="modal__para">
-            Whether it's for your next family movie night, a romantic date, or to discover your next binge-worthy TV series, Cinemations is here to help! With our vast database of
-            movies and TV shows, we guarantee you'll find all the info you need to pick your next hit movie!
-          </p>
+          <h3 className="modal__title">{modalTitle}</h3>
+          <p className="modal__para">{modalPara}</p>
         </div>
       </div>
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -24,7 +24,8 @@ function handleContactModal() {
   return (
     <>
     { 
-    aboutModal && <Modal />
+    aboutModal && <Modal modalTitle="Your Next Movie Is Our Mission" modalPara="Whether it's for your next family movie night, a romantic date, or to discover your next binge-worthy TV series, Cinemations is here to help! With our vast database of
+            movies and TV shows, we guarantee you'll find all the info you need to pick your next hit movie!" />
     }
     { 
     contactModal && <Modal />
