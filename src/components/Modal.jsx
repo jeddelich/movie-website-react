eb539a99d8841cc1703b0a538210a3992ff4b51e
jeddelich@gmail.com <jeddelich@gmail.com>
2026-01-28 13:24:53 -0500

    modal-close-added

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,16 @@
 import "./Modal.css";
 
-function Modal({ modalTitle, modalPara}) {
+function Modal({ modalTitle, modalPara, modalClose }) {
   return (
     <div className="modal">
       <div className="module__container">
         <div className="module__row">
-          <h3 className="modal__title">{modalTitle}</h3>
+          <div className="modal__header">
+            <h3 className="modal__title">{modalTitle}</h3>
+            <button className="modal__icon--wrapper" onClick={modalClose}>
+              <i className="fa-solid fa-xmark"></i>
+            </button>
+          </div>
           <p className="modal__para">{modalPara}</p>
         </div>
       </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -35,10 +35,10 @@ function handleContactModal() {
     <>
     { 
     aboutModal && <Modal modalTitle="Your Next Movie Is Our Mission" modalPara="Whether it's for your next family movie night, a romantic date, or to discover your next binge-worthy TV series, Cinemations is here to help! With our vast database of
-            movies and TV shows, we guarantee you'll find all the info you need to pick your next hit movie!" />
+            movies and TV shows, we guarantee you'll find all the info you need to pick your next hit movie!" modalClose={handleAboutModal}/>
     }
     { 
-    contactModal && <Modal modalTitle="Can't Find Your Movie Anywhere?" modalPara="We do our best to keep our library updated, but we need your help! Please contact us and let us know what we're missing!"/>
+    contactModal && <Modal modalTitle="Can't Find Your Movie Anywhere?" modalPara="We do our best to keep our library updated, but we need your help! Please contact us and let us know what we're missing!" modalClose={handleContactModal}/>
     }
     <nav>
       <div className="nav__side--left">
